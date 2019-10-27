6343bd1b0d6f9737ef84137f8baf4a97e6c1fcfe
StudentVM <studentVM2019@gmai.com>
2019-10-27 19:51:03 +0300

    done

--- iomanip/include/iomanip.hpp
+++ iomanip/include/iomanip.hpp
@@ -1,7 +1,7 @@
 /* Custom manipulators.
  * @file
- * @date 2018-08-05
- * @author Anonymous
+ * @date 2019-10-20
+ * @author Serega
  */
 
 #ifndef __IOMANIP_HPP__
@@ -9,16 +9,65 @@
 
 #include <iostream>
 
-//{ endm manipulator
+using namespace std;
 
-//}
+//----------------endm-----------------------------//
+ostream& endm(ostream& stream)
+{
+	stream << "[eol]\n";
+	return stream;
+}
+//-------------------------------------------------//
 
-//{ squares manipulator
 
-//}
+//--------------squares----------------------------//
 
-//{ add manipulator
+struct temp_sq {} squares;
+streambuf* buff;
 
-//}
+temp_sq& operator << (ostream& stream, temp_sq)
+{
+	buff = stream.rdbuf();
+	return squares;
+}
+template <typename T>
+ostream& operator << (temp_sq, T str)
+{
+	static ostream res(buff);
+	res << "[" << str << "]";
+	return res;
+}
+//-------------------------------------------------//
+
+//-----------add-----------------------------------//
+struct temp_add_1 {}add;
+
+template <typename T>
+struct temp_add_2 {
+	T temp;
+};
+template <typename T>
+temp_add_2<T> add_2;
+ 
+
+ temp_add_1& operator << (ostream& stream, temp_add_1){
+	 buff = stream.rdbuf();
+	 return add;
+ }
+
+template <typename T>
+temp_add_2<T>& operator << (temp_add_1,T temp){
+	add_2<T>.temp = temp;
+	return add_2<T>;
+}
+
+template <typename T>
+ostream& operator << (temp_add_2<T> t_add, T temp ){
+	static ostream str(buff);
+	str.flush();
+	str << (t_add.temp + temp);
+	return str;
+}
+//-------------------------------------------------//
 
 #endif // __IOMANIP_HPP__
