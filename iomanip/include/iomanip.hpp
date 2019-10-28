63e9c27c4de0b8c2362234f42a527fd25bd00f54
StudentVM <studentVM2019@gmai.com>
2019-10-28 15:53:18 +0300

    add CMakeList

--- iomanip/include/iomanip.hpp
+++ iomanip/include/iomanip.hpp
@@ -12,61 +12,28 @@
 using namespace std;
 
 //----------------endm-----------------------------//
-ostream& endm(ostream& stream)
-{
-	stream << "[eol]\n";
-	return stream;
-}
-//-------------------------------------------------//
+ostream& endm(ostream& stream){ return stream << "[eol]\n"; }
 
 
 //--------------squares----------------------------//
+struct temp_sq_1{} squares;
+struct temp_sq_2{ ostream& stream;};
 
-struct temp_sq {} squares;
-streambuf* buff;
-
-temp_sq& operator << (ostream& stream, temp_sq)
-{
-	buff = stream.rdbuf();
-	return squares;
-}
+temp_sq_2 operator<<(ostream& stream, temp_sq_1){ return {stream};}
 template <typename T>
-ostream& operator << (temp_sq, T str)
-{
-	static ostream res(buff);
-	res << "[" << str << "]";
-	return res;
-}
-//-------------------------------------------------//
+ostream& operator<<(temp_sq_2 temp, T data){ return temp.stream << '[' << data << ']'; }
+
 
 //-----------add-----------------------------------//
-struct temp_add_1 {}add;
+struct temp_add {} add;
+struct temp_add_1 {ostream& stream; };
+template<typename T> 
+struct temp_add_2 {ostream& stream; T data; };
 
-template <typename T>
-struct temp_add_2 {
-	T temp;
-};
-template <typename T>
-temp_add_2<T> add_2;
- 
- temp_add_1& operator << (ostream& stream, temp_add_1){
-	 buff = stream.rdbuf();
-	 return add;
- }
-
-template <typename T>
-temp_add_2<T>& operator << (temp_add_1,T temp){
-	add_2<T>.temp = temp;
-	return add_2<T>;
-}
-
-template <typename T>
-ostream& operator << (temp_add_2<T> t_add, T temp ){
-	static ostream str(buff);
-	str.flush();
-	str << (t_add.temp + temp);
-	return str;
-}
-//-------------------------------------------------//
+temp_add_1 operator<< (ostream& stream, temp_add){return {stream}; }
+template<typename T> 
+temp_add_2<T> operator<< (temp_add_1 temp, T data){ return{temp.stream, data}; }
+template<typename T>
+ostream& operator<< (temp_add_2<T> temp, T data){ return temp.stream << temp.data + data; }
 
 #endif // __IOMANIP_HPP__
